9af9cdd3d0357a685c710bc6de46f9ae421bd205
crpier <crpier42@gmail.com>
2023-05-20 01:42:07 +0300

    move to vite, re-do the site from scratch

--- web_homework/js/main.js
+++
@@ -1,8 +0,0 @@
-form = document.getElementById('contact_form');
-form_title = document.getElementById("form_title");
-
-function submission() {
-    console.log('Form Submitted!');
-    form_title.innerText = "Mesajul a fost trimis! Il vom citi!";
-    form.style.opacity = "0";
-}
